786bdea4e6c927fb8e37cdd8902b44ae385c0e44
1LolaOla <funmiolay1@gmail.com>
2023-08-14 23:31:54 +0000

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,6 +2,6 @@
 """Python function that returns the list of school"""
 
 def schools_by_topic(mongo_collection, topic):
-
+    """get schools by topic"""
     return mongo_collection.find({"topics":  {"$in": [topic]}})
 
